47790f21b2b5e7db0fdefbabc0510ae6280c71e5
mkchung <mk_free@qq.com>
2018-11-09 17:20:59 +0800

    Update AlipayPackage.java

--- android/src/main/java/com/yunpeng/alipay/AlipayPackage.java
+++ android/src/main/java/com/yunpeng/alipay/AlipayPackage.java
@@ -22,7 +22,7 @@ public class AlipayPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    //@Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
